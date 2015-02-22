514e9f43434a66f735b11073398256c6a7ffeb27
Michael Cohen <mcohen01@gmail.com>
2015-02-22 13:29:46 -0800

    add mixpanel and GA

--- index.php
+++ index.php
@@ -304,6 +304,7 @@ EOT;
 							currentTry + '&currentFrame=' + eval(currentFrame + 1) + '&correctAnswer=' + correctAnswer + '&feedback=' +
 							feed + '&percent=' + getScore() + '&numberOfQuestions=' + tutorialFrames.length + '&numberOfAttempts=' + eval(currentFrame + 1) + '&answeredCorrectly=' + numberCorrect, true);
 			xmlhttp.send(null);
+			mixpanel.track(tutorial, {correct: isCorrect, userAnswer: userAnswer});
 		}
 
 		function saveFinalScore() {
@@ -311,6 +312,7 @@ EOT;
 			var xmlhttp = xhr();
             xmlhttp.open("GET", parameters, true);
 			xmlhttp.send(null);
+			mixpanel.track(tutorial, {finalScore: getScore()});
 		}
 
 		function init() {
@@ -432,6 +434,24 @@ EOT;
 		}
 
 	</script>
+
+	<!-- mixpanel -->
+	<script type="text/javascript">(function(f,b){if(!b.__SV){var a,e,i,g;window.mixpanel=b;b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+			for(g=0;g<i.length;g++)f(c,i[g]);b._i.push([a,e,d])};b.__SV=1.2;a=f.createElement("script");a.type="text/javascript";a.async=!0;a.src="//cdn.mxpnl.com/libs/mixpanel-2-latest.min.js";e=f.getElementsByTagName("script")[0];e.parentNode.insertBefore(a,e)}})(document,window.mixpanel||[]);
+		mixpanel.init("fe760b8e932a191bdfef4fbc61b3435a");</script>
+
+	<!-- GA -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-5925374-15', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+
 </head>
 <body onLoad="init()" style="font-weight:bold; padding:20px;">
 <span id="frameNumber"></span><br>
